5404301e9e3803b5380ea0667ee7544621d896e6
GloriousEggroll <gloriouseggroll@gmail.com>
2026-06-09 19:01:44 -0600

    cleanup video rework patches

--- patches/video-rework.sh
+++ patches/video-rework.sh
@@ -20,9 +20,20 @@ apply_all_in_dir() {
     git reset --hard HEAD
     git clean -xdf
 
+    git revert --no-commit e813ca5771658b00875924ab88d525322e50d39f
+
+    echo "WINE: -WINEOPENXR- copy files into wine"
+    mkdir -p dlls/wineopenxr
+    cp -R ../wineopenxr/* dlls/wineopenxr/
 
 ### (2-5) WINE HOTFIX/BACKPORT SECTION ###
-    echo "WINE: -HOTFIX- Fix Smart Tee negotiation and V4L WoW64 media type marshaling"
+
+    # Separate OpenXR steam reliance
+    # https://github.com/GloriousEggroll/proton-ge-custom/issues/214
+    echo "WINE: -PENDING- add OpenXR patches"
+    apply_patch "../patches/wine-hotfixes/pending/0001-decouple-wineopenxr-from-steamvr-and-integrate-it-in.patch"
+
+    echo "WINE: -HOTFIX- Implement GE-Proton ffmpeg + winedmo only video playback rework patches"
     apply_all_in_dir "../patches/ge-video-rework/"
 
 
