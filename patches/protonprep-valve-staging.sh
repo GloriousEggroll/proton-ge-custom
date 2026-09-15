2e7dfa8949afeab380337fa3d3ba6d478eda5337
GloriousEggroll <gloriouseggroll@gmail.com>
2026-09-15 00:02:33 -0600

    patches: fix hades wine-wayland vulkan native crash

--- patches/protonprep-valve-staging.sh
+++ patches/protonprep-valve-staging.sh
@@ -339,6 +339,9 @@ apply_all_in_dir() {
     echo "WINE: -HOTFIX- Limit the extra Vulkan swapchain image workaround to DOOM"
     apply_patch "../patches/wine-hotfixes/pending/win32u-limit-extra-swapchain-image-to-doom.patch"
 
+    echo "WINE: -HOTFIX- Use three-image presentation modes for Hades on Wayland"
+    apply_patch "../patches/wine-hotfixes/pending/win32u-use-three-image-present-modes-for-hades-wayland.patch"
+
     echo "WINE: -HOTFIX- Retry virtual allocations with effective bounds after clearing native mappings"
     apply_patch "../patches/wine-hotfixes/pending/ntdll-retry-native-view-allocation-with-effective-range.patch"
 
